e63ac876e6cc206e2419500dbd021f2e891ffdbe
stackfing <stackfing@gmail.com>
2018-03-29 13:46:39 +0800

    vi

--- Other/video-cut.py
+++ Other/video-cut.py
@@ -87,6 +87,20 @@ import time
 import subprocess
 from subprocess import run
 
+def rename():
+        i=0
+        path="/home/fing/test/python/kuaishou/input";
+        filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
+        for files in filelist:#遍历所有文件
+            i=i+1
+            Olddir=os.path.join(path,files);#原来的文件路径                
+            if os.path.isdir(Olddir):#如果是文件夹则跳过
+                    continue;
+            filename=os.path.splitext(files)[0];#文件名
+            filetype=os.path.splitext(files)[1];#文件扩展名
+            Newdir=os.path.join(path,str(i)+filetype);#新的文件路径
+            os.rename(Olddir,Newdir)#重命名
+rename()
 
 class VideoDurationProcess:
     def __init__(self, filename):
